1d1f7496805d19efe72bd8fb68318d0c7b10a3cc
fangsj <1033595695@qq.com>
2017-10-27 17:21:14 +0800

    重构代码

---
+++ 2_CODE/longdetang/routes/admin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fangsj
+ * Date: 2017/10/27
+ * Time: 下午4:45
+ */
+Route::get('login', 'LoginController@showLoginForm')->name('admin.login');
+Route::post('login', 'LoginController@login');
+Route::any('logout', 'LoginController@logout');
+// 首页
+Route::any('/', 'IndexController@welcome');
+// 上传文件
+Route::post('upload', 'UploadController@upload');
+// 商品分类
+Route::get('prod/category', 'ProdCategoryController@list');
+Route::post('prod/category/add', 'ProdCategoryController@add');
+Route::any('prod/category/initEdit', 'ProdCategoryController@initEdit');
+Route::post('prod/category/edit', 'ProdCategoryController@edit');
+Route::post('prod/category/delete', 'ProdCategoryController@delete');
+Route::post('prod/category/enable', 'ProdCategoryController@enable');
+Route::post('prod/category/disable', 'ProdCategoryController@disable');
+// 商品二级分类
+Route::post('prod/category/second/add', 'ProdCategoryController@addSecondCategory');
+Route::any('prod/category/second/initEdit', 'ProdCategoryController@initEditSecondCategory');
+Route::post('prod/category/second/edit', 'ProdCategoryController@editSecondCategory');
+Route::post('prod/category/second/delete', 'ProdCategoryController@deleteSecondCategory');
+Route::post('prod/category/second/switch', 'ProdCategoryController@switchSecondCategory');
+// 视频库管理
+Route::get('video', 'VideoLibController@index');
+Route::post('video', 'VideoLibController@query');
+Route::post('video/delete', 'VideoLibController@delete');
+Route::get('video/add', 'VideoLibController@initAdd');
+Route::post('video/add', 'VideoLibController@add');
+Route::get('video/edit', 'VideoLibController@initEdit');
+Route::post('video/edit', 'VideoLibController@edit');
+Route::post('video/status', 'VideoLibController@switchStatus');
+// 艺人管理
+Route::get('artist', 'ArtistController@index');
+Route::post('artist', 'ArtistController@query');
+Route::post('artist/delete', 'ArtistController@delete');
+Route::get('artist/add', 'ArtistController@initAdd');
+Route::post('artist/add', 'ArtistController@add');
+Route::get('artist/edit', 'ArtistController@initEdit');
+Route::post('artist/edit', 'ArtistController@edit');
+// 轮播管理
+Route::get('banner', 'BannerController@index');
+Route::post('banner', 'BannerController@query');
+Route::post('banner/delete', 'BannerController@delete');
+Route::get('banner/add', 'BannerController@initAdd');
+Route::post('banner/add', 'BannerController@add');
+Route::get('banner/edit', 'BannerController@initEdit');
+Route::post('banner/edit', 'BannerController@edit');
--- 2_CODE/longdetang/routes/web.php
+++ 2_CODE/longdetang/routes/web.php
@@ -18,56 +18,6 @@ use App\Repositorys\Product\ProductRepository;
 //    return view('product.list');
 //});
 
-Route::group(['prefix' => 'admin','namespace' => 'Admin', 'middleware' => ['auth.admin:admin']],function ($router) {
-    // 登录
-    $router->get('login', 'LoginController@showLoginForm')->name('admin.login');
-    $router->post('login', 'LoginController@login');
-    $router->any('logout', 'LoginController@logout');
-    // 首页
-    $router->any('/', 'IndexController@welcome');
-    // 上传文件
-    $router->post('upload', 'UploadController@upload');
-    // 商品分类
-    $router->get('prod/category', 'ProdCategoryController@list');
-    $router->post('prod/category/add', 'ProdCategoryController@add');
-    $router->any('prod/category/initEdit', 'ProdCategoryController@initEdit');
-    $router->post('prod/category/edit', 'ProdCategoryController@edit');
-    $router->post('prod/category/delete', 'ProdCategoryController@delete');
-    $router->post('prod/category/enable', 'ProdCategoryController@enable');
-    $router->post('prod/category/disable', 'ProdCategoryController@disable');
-    // 商品二级分类
-    $router->post('prod/category/second/add', 'ProdCategoryController@addSecondCategory');
-    $router->any('prod/category/second/initEdit', 'ProdCategoryController@initEditSecondCategory');
-    $router->post('prod/category/second/edit', 'ProdCategoryController@editSecondCategory');
-    $router->post('prod/category/second/delete', 'ProdCategoryController@deleteSecondCategory');
-    $router->post('prod/category/second/switch', 'ProdCategoryController@switchSecondCategory');
-    // 视频库管理
-    $router->get('video', 'VideoLibController@index');
-    $router->post('video', 'VideoLibController@query');
-    $router->post('video/delete', 'VideoLibController@delete');
-    $router->get('video/add', 'VideoLibController@initAdd');
-    $router->post('video/add', 'VideoLibController@add');
-    $router->get('video/edit', 'VideoLibController@initEdit');
-    $router->post('video/edit', 'VideoLibController@edit');
-    $router->post('video/status', 'VideoLibController@switchStatus');
-    // 艺人管理
-    $router->get('artist', 'ArtistController@index');
-    $router->post('artist', 'ArtistController@query');
-    $router->post('artist/delete', 'ArtistController@delete');
-    $router->get('artist/add', 'ArtistController@initAdd');
-    $router->post('artist/add', 'ArtistController@add');
-    $router->get('artist/edit', 'ArtistController@initEdit');
-    $router->post('artist/edit', 'ArtistController@edit');
-    // 轮播管理
-    $router->get('banner', 'BannerController@index');
-    $router->post('banner', 'BannerController@query');
-    $router->post('banner/delete', 'BannerController@delete');
-    $router->get('banner/add', 'BannerController@initAdd');
-    $router->post('banner/add', 'BannerController@add');
-    $router->get('banner/edit', 'BannerController@initEdit');
-    $router->post('banner/edit', 'BannerController@edit');
-});
-
 //Auth::routes();
 //
 //Route::get('/home', 'HomeController@index')->name('home');
