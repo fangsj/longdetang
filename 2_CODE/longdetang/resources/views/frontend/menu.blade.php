45707f715acdfb7091284a80cf783005d39f48d6
fangsj <1033595695@qq.com>
2018-01-23 09:08:25 +0800

    1、增加康熙字典体

--- 2_CODE/longdetang/resources/views/frontend/menu.blade.php
+++ 2_CODE/longdetang/resources/views/frontend/menu.blade.php
@@ -1,8 +1,8 @@
 <ul class="clearfix">
-    <li><a href="{{url('/')}}">首页</a></li>
-    <li><a href="{{url('/article')}}">新事</a></li>
-    <li><a href="{{url('/prod')}}">作品</a></li>
-    <li><a href="{{url('/artist')}}">艺术家</a></li>
-    <li><a href="{{url('/video')}}">泽峰说</a></li>
-    <li><a href="{{url('/aboutus')}}">关于我们</a></li>
+    <li><a href="{{url('/')}}" style="font-family: KXZDT">首页</a></li>
+    <li><a href="{{url('/article')}}" style="font-family: KXZDT">新事</a></li>
+    <li><a href="{{url('/prod')}}" style="font-family: KXZDT">作品</a></li>
+    <li><a href="{{url('/artist')}}" style="font-family: KXZDT">艺术家</a></li>
+    <li><a href="{{url('/video')}}" style="font-family: KXZDT">泽峰说</a></li>
+    <li><a href="{{url('/aboutus')}}" style="font-family: KXZDT">关于我们</a></li>
 </ul>
