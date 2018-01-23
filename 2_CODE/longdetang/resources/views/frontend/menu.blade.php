f0b76b85b054e0380b69dc70f9f4794d792f000d
fangsj <1033595695@qq.com>
2018-01-23 09:09:11 +0800

    1、增加康熙字典体

--- 2_CODE/longdetang/resources/views/frontend/menu.blade.php
+++ 2_CODE/longdetang/resources/views/frontend/menu.blade.php
@@ -1,8 +1,8 @@
 <ul class="clearfix">
-    <li><a href="{{url('/')}}" style="font-family: KXZDT">首页</a></li>
-    <li><a href="{{url('/article')}}" style="font-family: KXZDT">新事</a></li>
-    <li><a href="{{url('/prod')}}" style="font-family: KXZDT">作品</a></li>
-    <li><a href="{{url('/artist')}}" style="font-family: KXZDT">艺术家</a></li>
-    <li><a href="{{url('/video')}}" style="font-family: KXZDT">泽峰说</a></li>
-    <li><a href="{{url('/aboutus')}}" style="font-family: KXZDT">关于我们</a></li>
+    <li><a href="{{url('/')}}" style="font-family: Kangxi">首页</a></li>
+    <li><a href="{{url('/article')}}" style="font-family: Kangxi">新事</a></li>
+    <li><a href="{{url('/prod')}}" style="font-family: Kangxi">作品</a></li>
+    <li><a href="{{url('/artist')}}" style="font-family: Kangxi">艺术家</a></li>
+    <li><a href="{{url('/video')}}" style="font-family: Kangxi">泽峰说</a></li>
+    <li><a href="{{url('/aboutus')}}" style="font-family: Kangxi">关于我们</a></li>
 </ul>
