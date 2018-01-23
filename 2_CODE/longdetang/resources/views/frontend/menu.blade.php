3ab7f92bc2f133db5e8e25160999543a4d33adff
fangsj <1033595695@qq.com>
2018-01-23 09:46:21 +0800

    1、解决ios康熙字典体无效

--- 2_CODE/longdetang/resources/views/frontend/menu.blade.php
+++ 2_CODE/longdetang/resources/views/frontend/menu.blade.php
@@ -1,8 +1,8 @@
 <ul class="clearfix">
-    <li><a href="{{url('/')}}" style="font-family: Kangxi">首頁</a></li>
-    <li><a href="{{url('/article')}}" style="font-family: Kangxi">新事</a></li>
-    <li><a href="{{url('/prod')}}" style="font-family: Kangxi">作品</a></li>
-    <li><a href="{{url('/artist')}}" style="font-family: Kangxi">兿術家</a></li>
-    <li><a href="{{url('/video')}}" style="font-family: Kangxi">澤鋒説</a></li>
-    <li><a href="{{url('/aboutus')}}" style="font-family: Kangxi">关于我們</a></li>
+    <li><a href="{{url('/')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">首頁</a></li>
+    <li><a href="{{url('/article')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">新事</a></li>
+    <li><a href="{{url('/prod')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">作品</a></li>
+    <li><a href="{{url('/artist')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">兿術家</a></li>
+    <li><a href="{{url('/video')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">澤鋒説</a></li>
+    <li><a href="{{url('/aboutus')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">关于我們</a></li>
 </ul>
