90858ae7e03a81c9d71e01291f0c4a299845b748
fangsj <1033595695@qq.com>
2018-01-23 21:51:16 +0800

    1、调整样式

--- 2_CODE/longdetang/resources/views/frontend/menu.blade.php
+++ 2_CODE/longdetang/resources/views/frontend/menu.blade.php
@@ -1,7 +1,7 @@
 <ul class="clearfix">
     <li><a href="{{url('/')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">首頁</a></li>
     <li><a href="{{url('/article')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">新事</a></li>
-    <li><a href="{{url('/prod')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">作品</a></li>
+    <li><a href="{{url('/prod')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">产品</a></li>
     <li><a href="{{url('/artist')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">兿術家</a></li>
     <li><a href="{{url('/video')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">澤鋒説</a></li>
     <li><a href="{{url('/aboutus')}}" style="font-family: Kangxi;text-rendering: optimizeLegibility;">关于我們</a></li>
