6ce5b3824519e263a63dcec047b461eddee03d71
fangsj <1033595695@qq.com>
2018-01-23 09:23:05 +0800

    1、增加康熙字典体

--- 2_CODE/longdetang/resources/views/frontend/menu.blade.php
+++ 2_CODE/longdetang/resources/views/frontend/menu.blade.php
@@ -1,8 +1,8 @@
 <ul class="clearfix">
-    <li><a href="{{url('/')}}" style="font-family: Kangxi">首页</a></li>
+    <li><a href="{{url('/')}}" style="font-family: Kangxi">首頁</a></li>
     <li><a href="{{url('/article')}}" style="font-family: Kangxi">新事</a></li>
     <li><a href="{{url('/prod')}}" style="font-family: Kangxi">作品</a></li>
-    <li><a href="{{url('/artist')}}" style="font-family: Kangxi">艺术家</a></li>
-    <li><a href="{{url('/video')}}" style="font-family: Kangxi">泽峰说</a></li>
-    <li><a href="{{url('/aboutus')}}" style="font-family: Kangxi">关于我们</a></li>
+    <li><a href="{{url('/artist')}}" style="font-family: Kangxi">兿術家</a></li>
+    <li><a href="{{url('/video')}}" style="font-family: Kangxi">澤鋒説</a></li>
+    <li><a href="{{url('/aboutus')}}" style="font-family: Kangxi">关于我們</a></li>
 </ul>
